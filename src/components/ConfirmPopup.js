bbecb5cf00a2cffcd7ae7a148b302a10e6be734b
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-01-16 13:02:02 -0800

    Create ConfirmPopup.js
    shows a modal to confirm or cancel when delete button is pressed for an issue

---
+++ src/components/ConfirmPopup.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Button } from "react-bootstrap";
+
+export default function ConfirmPopup({ handleDelete, setPopup }) {
+  return (
+    <>
+      <div className="popup-overlay"></div>
+      <div className="confirm-popup">
+        <div className="popup-body">
+          <p>This issue will be permanently deleted.</p>
+          <p>Are you sure?</p>
+          <Button
+            variant="secondary"
+            onClick={() => setPopup(false)}
+            style={{ position: "relative", float: "right", marginRight: "30%" }}
+          >
+            cancel
+          </Button>
+          <Button
+            variant="danger"
+            onClick={handleDelete}
+            style={{ position: "relative", float: "left" }}
+          >
+            DELETE
+          </Button>
+        </div>
+      </div>
+    </>
+  );
+}
