932e5c1d72df9308118a055edd8596cf18e1605a
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-01-16 14:21:12 -0800

    add icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,19 @@ import FullNavbar from "./FullNavbar";
 import { Link } from "react-scroll";
 import SmallNavbar from "./SmallNavbar";
 import { useWindowWidthAndHeight } from "../customHooks/CustomHooks";
+import { faBug } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Header = () => {
-  const [width, height] = useWindowWidthAndHeight();
+  const [width] = useWindowWidthAndHeight();
   return (
     <header>
       <div className="header-inner">
         <Link to="Home" className="logo nav-link">
+          <FontAwesomeIcon
+            icon={faBug}
+            style={{ fontSize: "1.5rem", marginRight: ".5rem" }}
+          />
           ISSUE TRACKER
         </Link>
         {width > 1000 ? (
